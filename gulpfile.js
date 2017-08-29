a97dc3785f38157306b10d9ae00ecd0779851dd1
student <student@STEP.LAN>
2017-08-29 20:55:01 +0300

    gulp task for minify css created

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
-$null 
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+
+ 
+gulp.task('min', function () {
+    gulp.src('dev/css/**/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('build/css'));
+});
